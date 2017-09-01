32a86a919e7834c24d192f024c71a53155ceb2c4
AdrielLimanthie <a.limanthie@yahoo.com>
2017-09-01 18:13:38 +0700

    Make the snake moveable with arrow keys

--- Snake/src/script.js
+++ Snake/src/script.js
@@ -1,20 +1,49 @@
+// Constants
+var KEY_LEFT = 37
+var KEY_UP = 38
+var KEY_RIGHT = 39
+var KEY_DOWN = 40
+
 // The game object
 var game = {
+    // Game attributes
     canvas: document.getElementById("gameCanvas2"),
+    prevTime: Date.now(),
+    player: {},
+    key: null,
+    // Game methods
     start: function() {
         this.canvas.width = 800
         this.canvas.height = 480
         this.context = this.canvas.getContext("2d")
-        this.interval = setInterval(updateGame, 20)
+
+        this.interval = setInterval(this.update.bind(this), 20)
+        document.addEventListener("keydown", function(e) {
+            var key = e.keyCode
+            if (key === KEY_LEFT || key === KEY_UP || key === KEY_RIGHT || key === KEY_DOWN) {
+                game.key = key
+            }
+        })
     },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    },
+    update: function() {
+        if (Date.now() - this.prevTime >= 500) {
+            this.prevTime += 500
+            if (this.key) {
+                this.player.direction = this.key
+                this.key = null
+            }
+
+            if (this.player.newPosition && this.player.update) {
+                this.clear()
+                this.player.newPosition()
+                this.player.update()
+            }
+        }
     }
 }
-
-var player
-
-var prevTime = Date.now()
 
 function snake(context, x, y) {
     this.width = 10
@@ -22,34 +51,28 @@ function snake(context, x, y) {
     this.x = x
     this.y = y
     // Direction - R = Right, L = Left, U = Up, D = Down
-    this.direction = "R"
+    this.direction = KEY_RIGHT
+
     this.update = function() {
         context.fillStyle = 'red'
         context.fillRect(this.x * 10, this.y * 10, this.width, this.height)
     }
     this.newPosition = function() {
-        if (this.direction === "R") {
+        if (this.direction === KEY_RIGHT) {
             this.x += 1
-        } else if (this.direction === "L") {
+        } else if (this.direction === KEY_LEFT) {
             this.x -= 1
-        } else if (this.direction === "U") {
+        } else if (this.direction === KEY_DOWN) {
             this.y += 1
-        } else if (this.direction === "L") {
+        } else if (this.direction === KEY_UP) {
             this.y -= 1
         }
     }
 }
 
-function updateGame() {
-    if (Date.now() - prevTime >= 500) {
-        prevTime += 500
-        game.clear()
-        player.newPosition()
-        player.update()
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function(event) {
+    // Player has to be instantiated after starting the game
+    // Otherwise, it will break the game
     game.start()
-    player = new snake(game.context, 1, 1)
+    game.player = new snake(game.context, 1, 1)
 })
