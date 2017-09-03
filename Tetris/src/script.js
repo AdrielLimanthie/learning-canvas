197a5812b6af196dd02bb7294a8354e15c30a47e
AdrielLimanthie <a.limanthie@yahoo.com>
2017-09-03 18:49:21 +0700

    Improve the game cycle

--- Tetris/src/script.js
+++ Tetris/src/script.js
@@ -6,23 +6,65 @@ var game = {
     height: 480,
     gameScreenX: 280,
     gameScreenWidth: 240,
+    prevTime: performance.now(),
+    initialized: false,
+    elements: [],
     // Game methods
-    start: function() {
-        this.canvas.width = this.width,
+    initialize: function() {
+        this.canvas.width = this.width
         this.canvas.height = this.height
         this.context = this.canvas.getContext("2d")
-
-        window.setInterval(this.update.bind(this), 20)
+        this.initialized = true
     },
-    update: function() {
+    start: function(timestamp) {
+        // Initialize the game only once
+        if (!this.initialized) {
+            this.initialize()
+        }
+
+        // Calculate time difference between this cycle and the previous cycle
+        var diff = timestamp - this.prevTime
+        this.prevTime = timestamp
+
+        // Update the game
+        this.update()
+
+        // Render the game
+        this.render()
+
+        // Go to the next cycle
+        window.requestAnimationFrame(this.start.bind(this))
+    },
+    update: function () {
+        // Update every game element
+        this.elements.forEach(function(element) {
+            element.update()
+        })
+    },
+    render: function() {
         // Render the game screen
         this.context.fillStyle = "#151515"
         this.context.fillRect(0, 0, this.width, this.height)
         this.context.fillStyle = "gainsboro"
         this.context.fillRect(this.gameScreenX, 0, this.gameScreenWidth, this.height)
+
+        // Render every game element
+        this.elements.forEach(function(element) {
+            element.render()
+        })
     }
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
     game.start()
 })
+
+// Want to make something like this
+//
+// function main() {
+//     var totem = Totem(800, 480, "totemGame")
+
+//     totem.addObject(0, 0, 30, 30, "blue")
+
+//     totem.start()
+// }
